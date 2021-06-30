39d87fff367df770e2a656b985cc82b53ca0a7fe
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-06-30 11:07:44 +0300

    Yes

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,7 @@
 
 const mc = require("minecraft-protocol");
 const illegalPackets = ["keep_alive", "login"];
-const mainFilter = (client, target, data, meta) => !illegalPackets.includes(meta.name) && !([meta.state, client.state, target.state].filter(x => x !== "play"));
+const mainFilter = (client, target, data, meta) => !illegalPackets.includes(meta.name) && !([meta.state, client.state, target.state].filter(x => x !== "play").length);
 
 /**
 * @typedef {function|boolean} ProxyFilterItem
@@ -25,8 +25,13 @@ class Proxy {
 	};
 	connect(client, opts = {}){
 		const { host = "localhost", port = 25565, username = (this.nick || client.username) } = opts;
-		this.createClient({ host, port, username }, true);
-		
+		let target = this.createClient({ host, port, username }, true);
+
+		client._proxycb = (data, meta) => this._pass.bind(this)(client, target, data, meta);
+		client.on("packet", client._proxycb);
+
+		target._proxycb = (data, meta) => this._pass.bind(this)(target, client, data, meta);
+		target.on("packet", target._proxycb);
 	};
 	setNick(username){
 		this.nick = username;
@@ -38,6 +43,13 @@ class Proxy {
 		if(!detached) this.targetClients.set(id, client);
 		if(setAsCurrent) this.targetClient = client;
 		return client;
+	};
+	_pass(client, target, data, meta){
+		if(mainFilter(client, target, data meta)) return;
+		let route = client.isServer ? "send" : "recieve";
+		if(this.filter[route+"All"] === false) return;
+		if(this.filter[route].has(meta.name)) {}; // TODO
+		target.write(meta.name, data);
 	};
 };
 
