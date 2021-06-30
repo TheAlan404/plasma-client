a29d7e4af2df7b61cd40611d9b4ade380b0b101d
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-06-30 10:51:49 +0300

    yse

--- src/proxy.js
+++ src/proxy.js
@@ -1,5 +1,9 @@
 /* Plasma Client | Proxy */
 
+const mc = require("minecraft-protocol");
+const illegalPackets = ["keep_alive", "login"];
+const mainFilter = (client, target, data, meta) => !illegalPackets.includes(meta.name) && !([meta.state, client.state, target.state].filter(x => x !== "play"));
+
 /**
 * @typedef {function|boolean} ProxyFilterItem
 * If it is a function, return an object for transforming the packet.
@@ -20,12 +24,20 @@ class Proxy {
 		this.nick = null;
 	};
 	connect(client, opts = {}){
-		// TODO: finish
 		const { host = "localhost", port = 25565, username = (this.nick || client.username) } = opts;
+		this.createClient({ host, port, username }, true);
 		
 	};
 	setNick(username){
 		this.nick = username;
+	};
+	createClient(opts = {}, setAsCurrent = true, detached = false){
+		let client = mc.createClient(opts);
+		let id = this.targetClients.size;
+		client.proxyid = id;
+		if(!detached) this.targetClients.set(id, client);
+		if(setAsCurrent) this.targetClient = client;
+		return client;
 	};
 };
 
